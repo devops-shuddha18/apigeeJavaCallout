c37df89204880ec23146c7dc981a2ab44f5e9172
Ayan Biswas <ayan9093@outlook.com>
2025-05-03 21:32:00 +0530

    created new jar

--- src/main/java/com/bupa/javacallout/ApigeeCallout.java
+++ src/main/java/com/bupa/javacallout/ApigeeCallout.java
@@ -5,19 +5,44 @@ import com.apigee.flow.execution.ExecutionResult;
 import com.apigee.flow.execution.spi.Execution;
 import com.apigee.flow.message.MessageContext;
 
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Scanner;
+
 public class ApigeeCallout implements Execution {
 
+    @Override
     public ExecutionResult execute(MessageContext messageContext, ExecutionContext executionContext) {
-
         try {
+            // Get request headers
+            Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+            for (String headerName : messageContext.getMessage().getHeaderNames()) {
+                headers.put(headerName, messageContext.getMessage().getHeader(headerName));
+            }
 
-            messageContext.setVariable("foo", "Hello World!");
+            /*// Get query parameters
+            Map<String, String> queryParams = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+            Set<String> paramNames = messageContext.getMessage().getQueryParams().getParameterNames();
+            for (String param : paramNames) {
+                queryParams.put(param, messageContext.getMessage().getQueryParams().get(param));
+            }*/
+
+            // Get request body as string
+            InputStream inStream = messageContext.getMessage().getContentAsStream();
+            String body = (inStream != null) ? new Scanner(inStream, "UTF-8").useDelimiter("\\A").next() : "";
+
+            // Log all the data (or set it as variables if you prefer)
+            messageContext.setVariable("javaCallout.headers", headers.toString());
+            //messageContext.setVariable("javaCallout.queryParams", queryParams.toString());
+            messageContext.setVariable("javaCallout.body", body);
 
             return ExecutionResult.SUCCESS;
 
         } catch (Exception e) {
+            messageContext.setVariable("javaCallout.error", e.getMessage());
             return ExecutionResult.ABORT;
         }
     }
-
 }
